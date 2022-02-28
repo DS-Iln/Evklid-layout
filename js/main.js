059d3162dae7a253934debbd00c23cd210cd838d
Denis Ilyin <DenisIlin2@yandex.ru>
2022-02-28 17:44:58 +0300

    Important update
    Header animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    // Declare DOM elements
+    const navBar = document.querySelector('.header-navbar'),
+          searchBar = document.querySelector('.header-searchbar'),
+          openSearchbutton = document.querySelector('#open-search'),
+          closeSearchbutton = document.querySelector('#close-search');
+    // Declare variables
+    // let
 
+    // Initialize slider
     const swiper = new Swiper('.swiper', {
         loop: false,
         pagination: {
@@ -9,4 +16,22 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     });
 
+    // Open search-bar
+    openSearchbutton.addEventListener('click', () => {
+        if (navBar.classList.contains('opened')) navBar.classList.remove('opened');
+        navBar.classList.add('navbar-outed');
+        navBar.style.display = 'none';
+        if (searchBar.classList.contains('searchbar-outed')) searchBar.classList.remove('searchbar-outed');
+        searchBar.style.display = 'block';
+        searchBar.classList.add('opened');
+    });
+    // Close search-bar
+    closeSearchbutton.addEventListener('click', () => {
+        if (searchBar.classList.contains('opened')) searchBar.classList.remove('opened');
+        searchBar.classList.add('searchbar-outed');
+        searchBar.style.display = 'none';
+        if (navBar.classList.contains('navbar-outed')) navBar.classList.remove('navbar-outed');
+        navBar.style.display = 'flex';
+        navBar.classList.add('opened');
+    });
 })
