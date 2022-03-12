49cc255199d5e3f89ae4af0a02682fbeac714922
MoonFlower1773 <73419296+MoonFlower1773@users.noreply.github.com>
2022-03-12 13:43:26 +0300

    Important update

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const navBar = document.querySelector('.header-navbar'),
           searchBar = document.querySelector('.header-searchbar'),
           openSearchbutton = document.querySelector('#open-search'),
-          closeSearchbutton = document.querySelector('#close-search');
+          closeSearchbutton = document.querySelector('#close-search'),
+          burgeropenButton = document.querySelector('#burger-open');
     // Declare variables
     // let
 
@@ -20,18 +21,25 @@ document.addEventListener('DOMContentLoaded', () => {
     openSearchbutton.addEventListener('click', () => {
         if (navBar.classList.contains('opened')) navBar.classList.remove('opened');
         navBar.classList.add('navbar-outed');
-        navBar.style.display = 'none';
+        navBar.classList.add('_hidden');
         if (searchBar.classList.contains('searchbar-outed')) searchBar.classList.remove('searchbar-outed');
-        searchBar.style.display = 'block';
+        searchBar.classList.remove('_hidden');
         searchBar.classList.add('opened');
     });
     // Close search-bar
     closeSearchbutton.addEventListener('click', () => {
         if (searchBar.classList.contains('opened')) searchBar.classList.remove('opened');
         searchBar.classList.add('searchbar-outed');
-        searchBar.style.display = 'none';
+        searchBar.classList.add('_hidden');
         if (navBar.classList.contains('navbar-outed')) navBar.classList.remove('navbar-outed');
-        navBar.style.display = 'flex';
+        navBar.classList.remove('_hidden');
         navBar.classList.add('opened');
     });
+
+    // Open Burger
+    burgeropenButton.addEventListener('click', () => {
+
+    });
+    // Close burger
+    // ...
 })
